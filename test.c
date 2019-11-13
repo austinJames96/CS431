2e4adfdf00ac7f01292148fec6abcd13473e5b53
austinJames96 <57360655+austinJames96@users.noreply.github.com>
2019-11-13 15:27:52 -0500

    Example using pthread

--- test.c
+++ test.c
@@ -13,12 +13,29 @@ int main(int argc, char *argv[]){
         exit(1);
     }
     pthread_t p1, p2;
-    int rc; printf("main: begin\n");
- 
+    int rc; 
+    int num_threads = 26;
+    printf("main: begin\n");
+/*
     pthread_create(&p1, NULL, mythread, "A");
     pthread_create(&p2, NULL, mythread, "B");
     pthread_join(p1, NULL);
     pthread_join(p2, NULL);
+*/
+    //printf("%d", (char *) "A");
+    //char starting = (int *) 65;
+    
+    pthread_t threads[num_threads];
+    for(int i = 0; i<num_threads; i++){
+        pthread_create(&threads[i], NULL, mythread, "A");
+    }
+
+
+    for (int i = 0; i<num_threads; i++){
+        pthread_join(threads[i], NULL);
+    }
+
+
     printf("main: end\n");
     
     return 0;
